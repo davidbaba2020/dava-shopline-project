96d88285ea468694afff17cf2cd99e3b48ed5987
David Baba <davidbabasalifu2020@gmail.com>
2021-10-21 17:39:54 +0100

    Created my spring project to commence the back end development

---
+++ backend/dava-shopline/src/main/java/com/davacom/davashopline/DavaShoplineApplication.java
@@ -0,0 +1,13 @@
+package com.davacom.davashopline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavaShoplineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DavaShoplineApplication.class, args);
+    }
+
+}
---
+++ backend/dava-shopline/src/test/java/com/davacom/davashopline/DavaShoplineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davacom.davashopline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavaShoplineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
